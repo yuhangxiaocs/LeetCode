f2a341d021984c95cffd780faab58e0b98ecdaa1
yuhangxiaocs <1248618975@qq.com>
2019-12-24 00:52:26 +0800

    update solution

--- Solution_1025.java
+++ Solution_1025.java
@@ -1,21 +1,22 @@
 public class Solution_1025 {
 
     public boolean divisorGame(int N) {
-        if (N == 1)
-            return false;
         boolean[] dp = new boolean[N + 1];
+        dp[0] = false;
         dp[1] = false;
-        dp[2] = true;
-        for (int i = 3; i <= N; i++) {
+        for (int i = 2; i <= N; i++) {
             dp[i] = false;
             for (int j = 1; j < i; j++)
-                if (i % j == 0 && dp[i - j] == false) {
+                if (dp[i - j] == false && i % j == 0) {
                     dp[i] = true;
                     break;
                 }
-
         }
         return dp[N];
+    }
+
+    public boolean divisorGame(int N, int second_solution) {
+        return N % 2 == 0;
     }
 
     public static void main(String[] args) {
