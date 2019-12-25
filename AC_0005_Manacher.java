047fbe866d1f8cd7ccc532844c7343b4d99bb281
yuhangxiaocs <1248618975@qq.com>
2019-12-25 23:25:18 +0800

    finish manacher algorithm

--- AC_0005_Manacher.java
+++ AC_0005_Manacher.java
@@ -1,43 +1,52 @@
 public class AC_0005_Manacher {
 
-    public static String Manacher(String string) {
+    public static String Manacher(String s) {
 
-        StringBuilder s = new StringBuilder();
-        for (int i = 0; i < string.length(); i++) {
-            s.append("#");
-            s.append(string.charAt(i));
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            newString.append("#");
+            newString.append(s.charAt(i));
         }
-        s.append("#");
-        int[] p = new int[s.length()];
+        newString.append("#");
+        int[] p = new int[newString.length()];
         /*
-         * C center
-         * R right border
-         * rad radius
+         * C: center
+         * R: right border
+         * rad: radius
          */
         int C = 0, R = -1, rad;
-        for (int i = 0; i < s.length(); i++) {
+        int maxC = -99, maxRad = -99;// in order to find the specfic solution
+        for (int i = 0; i < newString.length(); i++) {
             if (i <= R)
                 rad = Math.min(p[2 * C - i], R - i);
             else
                 rad = 0;
             // extend right border
-            while (i + rad < s.length() && i - rad >= 0 && s.charAt(i + rad) == s.charAt(i - rad))
+            while (i + rad + 1 < newString.length() && i - rad - 1 >= 0
+                    && newString.charAt(i + rad + 1) == newString.charAt(i - rad - 1))
                 rad++;
 
             // update result
             p[i] = rad;
             // check if update right border R and center C
-            if (R < i + rad - 1) {
-                R = i + rad - 1;
+            if (R < i + rad) {
+                R = i + rad;
                 C = i;
+                if (rad > maxRad) {
+                    maxRad = rad;
+                    maxC = C;
+                }
             }
         }
-
-        System.out.print(s);
-        return "";
+        int left = maxC - maxRad;
+        return s.substring(left / 2, left / 2 + maxRad);
     }
 
     public static void main(String[] args) {
-        Manacher("abbq");
+        String s = Manacher("aba");
+        System.out.print(s);
     }
 }
+
+// 0 1 0 1 4 1 0 1 0
+// 0 1 0 1 4 1 0 1 0
