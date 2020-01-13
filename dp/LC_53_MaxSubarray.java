51fc7b11675e1d855b16f8ee84d90d3dd5d96579
yuhangxiaocs <1248618975@qq.com>
2020-01-13 15:57:11 +0800

    。

--- dp/LC_53_MaxSubarray.java
+++ dp/LC_53_MaxSubarray.java
@@ -1,16 +1,17 @@
 public class LC_53_MaxSubarray {
 
-    // 要求连续 如果都是负数 返回0
+    // 要求连续 如果都是负数 返回负数
     static public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0)
             return 0;
         int len = nums.length;
-        int maxSum = 0x80000000;
+        int maxSum = Integer.MIN_VALUE;
         int sum = 0;
         for (int i = 0; i < len; i++) {
             sum += nums[i];
             if (sum > maxSum)
                 maxSum = sum;
+                // 这样设计就既可以统计整数可以统计负数
             if (sum < 0)
                 sum = 0;
         }
