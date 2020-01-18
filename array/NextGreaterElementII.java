f3c3b19ff0effd102f73579e07119442a632858d
yuhangxiaocs <1248618975@qq.com>
2020-01-18 23:48:24 +0800

    。。

--- array/NextGreaterElementII.java
+++ array/NextGreaterElementII.java
@@ -8,6 +8,10 @@ public class NextGreaterElementII {
 
     /**
      * 用的还是1的思路，只不过这里map中的key放的是下标以防止重复 否则就会覆盖了
+     * 
+     * 
+     * 
+     * 之所以没用循环数组的%性质 是绝对浪费时间 但是这样一来就要用两倍的数组 空间就大了 实际上可以的
      */
     static public int[] nextGreaterElements(int[] nums) {
         int[] longnums = new int[nums.length * 2];
@@ -27,6 +31,22 @@ public class NextGreaterElementII {
 
         return nums;
     }
+    
+    /*
+     * 高票答案思路
+     *  public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length, next[] = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> stack = new Stack<>(); // index stack
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n]; 
+            while (!stack.isEmpty() && nums[stack.peek()] < num)
+                next[stack.pop()] = num;
+            if (i < n) stack.push(i);
+        }   
+        return next;
+    }
+     */
 
     // [120, 11, 120, 120, 123, 123, -1, 100, 100, 100]
     // [120, 11, 120, 120, 123, 123, -1, 100, 100, 100]
--- dp/MinimumCostTreeFromLeafValues.java
+++ dp/MinimumCostTreeFromLeafValues.java
@@ -1,32 +1,28 @@
+import java.util.Stack;
+
 public class MinimumCostTreeFromLeafValues {
 
     static public int mctFromLeafValues(int[] arr) {
-        if (arr == null || arr.length < 2)
-            return 0;
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        // 放一个哨兵
+        stack.push(Integer.MAX_VALUE);
+        // 用stack维护一个递减的顺序
+        for (int a : arr) {
+            while (a >= stack.peek()) {
+                // 中间的就可以移除了 因为它是要求最大的乘积 所以这里没有用了
+                int mid = stack.pop();
+                res += mid * Math.min(stack.peek(), a);
+            }
 
-        // 到i为止最大的元素
-        int[] max = new int[arr.length];
-        int maxV = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxV)
-                maxV = arr[i];
-            max[i] = maxV;
+            stack.push(a);
         }
 
-        int[] dp = new int[arr.length];
+        // 必须要是一个pop一个peek
+        while (stack.size() > 2)
+            res += stack.pop() * stack.peek();
 
-        dp[0] = 0;
-        dp[1] = arr[0] * arr[1];
-
-        for (int i = 2; i < arr.length; i++) {
-
-            dp[i] = Math.min(dp[i - 1] + max[i - 1] * arr[i],
-                    dp[i - 2] + max[i - 2] * Math.max(arr[i], arr[i - 1]) + arr[i - 1] * arr[i]);
-        }
-        for (int i : dp) {
-            System.out.print(i + " ");
-        }
-        return dp[arr.length - 1];
+        return res;
     }
 
     public static void main(String[] args) {
@@ -35,3 +31,38 @@ public class MinimumCostTreeFromLeafValues {
         System.out.println(result);
     }
 }
+
+/*
+    * 这个思路不对哦！！！ 
+
+   static public int mctFromLeafValues(int[] arr) {
+       if (arr == null || arr.length < 2)
+           return 0;
+
+       // 到i为止最大的元素
+       int[] max = new int[arr.length];
+       int maxV = Integer.MIN_VALUE;
+       for (int i = 0; i < arr.length; i++) {
+           if (arr[i] > maxV)
+               maxV = arr[i];
+           max[i] = maxV;
+       }
+
+       int[] dp = new int[arr.length];
+
+       dp[0] = 0;
+       dp[1] = arr[0] * arr[1];
+
+       for (int i = 2; i < arr.length; i++) {
+
+           dp[i] = Math.min(dp[i - 1] + max[i - 1] * arr[i],
+                   dp[i - 2] + max[i - 2] * Math.max(arr[i], arr[i - 1]) + arr[i - 1] * arr[i]);
+       }
+       for (int i : dp) {
+           System.out.print(i + " ");
+       }
+       return dp[arr.length - 1];
+   }
+   
+   
+*/
